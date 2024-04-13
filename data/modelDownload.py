fe7120af22839fe4e4ce7c9cac1b437b95852567
EduardoNeville <eduardoneville82@gmail.com>
2024-04-13 13:03:52 +0200

    changes to modelDownload

--- data/modelDownload.py
+++ data/modelDownload.py
@@ -2,7 +2,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def tryRunningModel(model, tokenizer):
     text = input()
-
+    model = AutoModelForCausalLM.from_pretrained("ai21labs/Jamba-v0.1", trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained("ai21labs/Jamba-v0.1")
     inputs = tokenizer(text, return_tensors="pt")
     outputs = model(**inputs, max_new_tokens=500)
 
@@ -10,19 +11,21 @@ def tryRunningModel(model, tokenizer):
 
 
 def main():
-    models_list = ["mistralai/Mixtral-8x7B-v0.1"]#, "ai21labs/Jamba-v0.1", "CohereForAI/c4ai-command-r-plus"]
+    #models_list = ["mistralai/Mixtral-8x7B-v0.1"]#, "ai21labs/Jamba-v0.1", "CohereForAI/c4ai-command-r-plus"]
+
     tokenizer = None
+    model = None
 
-    for model_id in models_list:
-        print(f"Downloading {model_id}...")
-        model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
+    #for model_id in models_list:
+    #    print(f"Downloading {model_id}...")
+    #    model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
+    #    tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-        print(f"Model: {model_id} has been downloaded!")
+    #    print(f"Model: {model_id} has been downloaded!")
 
-    print(f"Finished downloading: \n {models_list}")
+    #print(f"Finished downloading: \n {models_list}")
 
-    tryRunningModel(models_list[0], tokenizer)
+    tryRunningModel(model, tokenizer)
 
 if __name__ == '__main__':
     main()
--- output_aggregator.py
+++ output_aggregator.py
@@ -1,8 +1,16 @@
 import json
 import os
 from pathlib import Path
+import multiprocessing as mp
 
 def ethicsAggregator(model_name: str)-> bool:
+    pathOuput = Path("benchmarks/ethics/results")
+    lstEvals = ["commonsense","commonsense-hard","deontology","deontology-hard","justice","justice-hard","utilitarianism","utilitarianism-hard","virtue","virtue-hard"]
+    try:
+        for eval in lstEvals:
+            with open(pathOuput/f"{eval}/f"{model_name}".json", 'r') as file:
+                data = json.load(file)
+                print(data)
 
     return True
 
@@ -27,8 +35,17 @@ def llmRulesAggregator(model_name: str)-> bool:
         return False
 
 def fileAggregator(model_name: str)-> bool:
+    # Strip model name 
+    components = model_name.split('/')
+    benchmarksAggregators = [ethicsAggregator, theoryOfMindAggregator, machiavelliAggregator, llmRulesAggregator] 
+    try:
+        ctx = mp.get_context('spawn')
+        with ctx.Pool() as pool:
+            print("Tokenizing data...")
+            results = list(pool.imap(tokenize_and_print, [(i, dataset[i]['text']) for i in range(dataLen)]), total=len(benchmarksAggregators)))
 
-    return True
+    except FileNotFoundError:
+        return True
 
 def main():
     print(f"Current working directory: {os.getcwd()}")
