b99d0852bfb607eb2c88f4d6a3eec74eeb12b50d
EduardoNeville <eduardoneville82@gmail.com>
2024-04-13 11:29:00 +0200

    added changes to run

--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-#python eval.py mistralai/Mistral-7B-Instruct-v0.2/
-python eval.py vietgpt-archive/gpt2-150M
-=======
 python eval.py ai21labs/Jamba-v0.1
->>>>>>> 5cd7e42 (changes to run)
